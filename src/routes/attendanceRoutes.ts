bf6c45c1cf0ffa27d1793a62319d6ab929c9c7b7
PrannavP <ramsarilekhi@gmail.com>
2025-07-27 23:41:12 +0545

    Complete attendance page endpoint for HR attendance page

--- src/routes/attendanceRoutes.ts
+++ src/routes/attendanceRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { checkIn, checkOut, getAttendance } from "../controllers/attendance.controller";
+import { checkIn, checkOut, getAttendance, getAllEmployeeAttendance } from "../controllers/attendance.controller";
 
 const router = express.Router();
 
 router.post("/checkin", checkIn);
 router.post("/checkout", checkOut);
 router.get("/attendance/:emp_id", getAttendance);
+router.get("/hr/EmployeesAttendance", getAllEmployeeAttendance);
 
 export default router;
